aadfcd7815f96ec6ef7c6e79bc2521b74f67d0ca
unknown <habtamuanbese@gmail.com>
2024-12-12 16:33:31 -0600

    updated

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-e538c/us-central1/api"
+  baseURL: "https://amazonapi-oe4e.onrender.com",
   // baseURL: "https://amazon-api-deploy-st1d.onrender.com/",
 });
 
